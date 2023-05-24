78e743aabf6855f93c74617dce9ca5ff4c377ac3
amirwessam <121111555+amirwessam@users.noreply.github.com>
2023-05-24 17:43:16 +0300

    Update api.js

--- routes/public/api.js
+++ routes/public/api.js
@@ -2,85 +2,81 @@ const { isEmpty } = require("lodash");
 const { v4 } = require("uuid");
 const db = require("../../connectors/db");
 const roles = require("../../constants/roles");
+const {getSessionToken}=require('../../utils/session')
+const getUser = async function (req) {
+  const sessionToken = getSessionToken(req);
+  if (!sessionToken) {
+    return res.status(301).redirect("/");
+  }
+  console.log("hi",sessionToken);
+  const user = await db
+    .select("*")
+    .from("se_project.sessions")
+    .where("token", sessionToken)
+    .innerJoin(
+      "se_project.users",
+      "se_project.sessions.userid",
+      "se_project.users.id"
+    )
+    .innerJoin(
+      "se_project.roles",
+      "se_project.users.roleid",
+      "se_project.roles.id"
+    )
+   .first();
+
+  console.log("user =>", user);
+  user.isNormal = user.roleid === roles.user;
+  user.isAdmin = user.roleid === roles.admin;
+  user.isSenior = user.roleid === roles.senior;
+  console.log("user =>", user)
+  return user;
+};
+
 module.exports = function (app) {
-app.post("/api/v1/user", async function (req, res) {
-
-    // Check if user already exists in the system
-    const userExists = await db
-      .select("*")
-      .from("se_project.users")
-      .where("email", req.body.email);
-    if (!isEmpty(userExists)) {
-      return res.status(400).send("user exists");
-    }
-
-    const newUser = {
-      firstname: req.body.firstName,
-      lastname: req.body.lastName,
-      email: req.body.email,
-      password: req.body.password,
-      roleid: roles.user,
-    };
+  // example
+  app.get("/api/v1/zones", async function (req, res) {
     try {
-      const user = await db("se_project.users").insert(newUser).returning("*");
-
-      return res.status(200).json(user );
+      const zones = await db.select("*").from("se_project.zones");
+      return res.status(200).json(zones);
     } catch (e) {
       console.log(e.message);
-      return res.status(400).send("Could not register user");
+      return res.status(400).send("Could not get zones");
     }
   });
-
-  // Register HTTP endpoint to create new user
-  app.post("/api/v1/user/login", async function (req, res) {
-    // get users credentials from the JSON body
-    const { email, password } = req.body;
-    if (!email) {
-      // If the email is not present, return an HTTP unauthorized code
-      return res.status(400).send("email is required");
-    }
-    if (!password) {
-      // If the password is not present, return an HTTP unauthorized code
-      return res.status(400).send("Password is required");
-    }
-
-    // validate the provided password against the password in the database
-    // if invalid, send an unauthorized code
-    const user = await db
-      .select("*")
-      .from("se_project.users")
-      .where("email", email)
-      .first();
-    if (isEmpty(user)) {
-      return res.status(400).send("user does not exist");
-    }
-
-    if (user.password !== password) {
-      return res.status(401).send("Password does not match");
-    }
-
-    // set the expiry time as 15 minutes after the current time
-    const token = v4();
-    const currentDateTime = new Date();
-    const expiresat = new Date(+currentDateTime + 900000); // expire in 15 minutes
-
-    // create a session containing information about the user and expiry time
-    const session = {
-      userid: user.id,
-      token,
-      expiresat,
-    };
+  app.post("/api/v1/payment/subscription", async function (req, res) {
     try {
-      await db("se_project.sessions").insert(session);
-      // In the response, set a cookie on the client with the name "session_cookie"
-      // and the value as the UUID we generated. We also set the expiration time.
-      return res
-        .cookie("session_token", token, { expires: expiresat })
-        .status(200)
-        .send("login successful");
+      const user = await getUser(req);
+      const { paymentMethodId, priceId } = req.body;
+      const customer = await db
+        .select("*")
+        .from("se_project.customers")
+        .where("userid", user.id)
+        .first();
+      if (!customer) {
+        return res.status(400).send("Customer not found");
+      }
+      const subscription = await stripe.subscriptions.create({
+        customer: customer.stripeid,
+        items: [{ price: priceId }],
+        default_payment_method: paymentMethodId,
+      });
+      return res.status(200).json(subscription);
     } catch (e) {
       console.log(e.message);
-      return res.status(400).send("Could not register user");
+      return res.status(400).send("Could not create subscription");
     }
+  });
+  app.get("/users", async function (req, res) {
+    try {
+       const user = await getUser(req);
+      const users = await db.select('*').from("se_project.users")
+        
+      return res.status(200).json(users);
+    } catch (e) {
+      console.log(e.message);
+      return res.status(400).send("Could not get users");
+    }
+   
   });
 }
