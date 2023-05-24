58faaacf63eebcd25eaecc4361774191e728445f
Mostafa2873 <121504738+Mostafa2873@users.noreply.github.com>
2023-05-24 21:23:04 +0300

    push one

--- routes/private/api.js
+++ routes/private/api.js
@@ -35,6 +35,103 @@ const getUser = async function (req) {
 
 module.exports = function (app) {
   // example
+  const createRoute = async function (req, res) {
+    try {
+      const user = await getUser(req);
+      
+      // Check if the user is an admin
+      if (!user.isAdmin) {
+        return res.status(403).send("Only admin can create routes.");
+      }
+  
+      const { newStationId, connectedStationId, routeName } = req.body;
+  
+      // Check if the required fields are provided
+      if (!newStationId || !connectedStationId || !routeName) {
+        return res.status(400).send("Invalid request. Missing required fields.");
+      }
+  
+      // Perform any additional validations if needed
+  
+      // Create the route in the database
+      const route = await db("se_project.routes").insert({
+        new_station_id: newStationId,
+        connected_station_id: connectedStationId,
+        route_name: routeName
+      });
+  
+      return res.status(201).json({ message: "Route created successfully." });
+    } catch (e) {
+      console.log(e.message);
+      return res.status(500).send("Internal Server Error");
+    }
+  };
+  
+  module.exports = function (app) {
+    app.post("/api/v1/route", createRoute);
+  
+    // Rest of your routes
+  };
+  
+  const updateRoute = async function (req, res) {
+    try {
+      const user = await getUser(req);
+  
+      // Check if the user is an admin
+      if (!user.isAdmin) {
+        return res.status(403).send("Only admin can update routes.");
+      }
+  
+      const routeId = req.params.routeId;
+      const { routeName } = req.body;
+  
+      // Check if the required fields are provided
+      if (!routeName) {
+        return res.status(400).send("Invalid request. Missing required fields.");
+      }
+  
+      // Perform any additional validations if needed
+  
+      // Update the route name in the database
+      const updatedRoute = await db("se_project.routes")
+        .where("id", routeId)
+        .update({ route_name: routeName });
+  
+      // Check if the route was successfully updated
+      if (updatedRoute === 0) {
+        return res.status(404).send("Route not found.");
+      }
+  
+      return res.status(200).json({ message: "Route updated successfully." });
+    } catch (e) {
+      console.log(e.message);
+      return res.status(500).send("Internal Server Error");
+    }
+  };
+  
+  module.exports = function (app) {
+    app.put("/api/v1/route/:routeId", updateRoute);
+  
+    // Rest of your routes
+  };
+  
+
+
+
+
+  
+module.exports = function (app) {
+  app.post("/manage/routes", createRoute);
+};
+
+  
+  
+  
+  
+  
+  
+  
+  
   app.get("/users", async function (req, res) {
     try {
        const user = await getUser(req);
