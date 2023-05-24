f0e1646e36bf24ffd65e5fa5ef044bcd8cd2b3d5
amirwessam <121111555+amirwessam@users.noreply.github.com>
2023-05-24 17:53:17 +0300

    Update api.js

--- routes/public/api.js
+++ routes/public/api.js
@@ -67,6 +67,68 @@ module.exports = function (app) {
       return res.status(400).send("Could not create subscription");
     }
   });
+  app.post("/api/v1/payment/ticket", async function (req, res) {
+    try {
+      const user = await getUser(req);
+      const { paymentMethodId, priceId } = req.body;
+      const customer = await db
+        .select("*")
+        .from("se_project.customers")
+        .where("userid", user.id)
+        .first();
+      if (!customer) {
+        return res.status(400).send("Customer not found");
+      }
+      const paymentIntent = await stripe.paymentIntents.create({
+        customer: customer.stripeid,
+        payment_method: paymentMethodId,
+        amount: 1000,
+        currency: "EGP",
+        confirmation_method: "manual",
+        confirm: true,
+      });
+      return res.status(200).json(paymentIntent);
+    } catch (e) {
+      console.log(e.message);
+      return res.status(400).send("Could not create payment intent");
+    }
+  });
+  app.post("/api/v1/tickets/purchase/subscription", async function (req, res) {
+    try {
+      const user = await getUser(req);
+      const { subscriptionId } = req.body;
+      const customer = await db
+        .select("*")
+        .from("se_project.customers")
+        .where("userid", user.id)
+        .first();
+      if (!customer) {
+        return res.status(400).send("Customer not found");
+      }
+      const subscription = await stripe.subscriptions.retrieve(subscriptionId);
+      if (!subscription) {
+        return res.status(400).send("Subscription not found");
+      }
+      const ticket = await db
+        .insert({
+          id: v4(),
+          userid: user.id,
+          subscriptionid: subscription.id,
+          status: "active",
+          createdat: new Date(),
+          updatedat: new Date(),
+        })
+        .into("se_project.tickets")
+        .returning("*")
+        .then((rows) => rows[0]);
+      return res.status(200).json(ticket);
+    } catch (e) {
+      console.log(e.message);
+      return res.status(400).send("Could not purchase subscription");
+    }
+  });
+  
+  
   app.get("/users", async function (req, res) {
     try {
        const user = await getUser(req);
