e1ab1dd5338f4916e6a571a11d87ddb05af212a9
mostafaaboeita <mostafa.sat40@gmail.com>
2023-05-25 06:39:49 -0700

    3 api done with testing

--- routes/private/api.js
+++ routes/private/api.js
@@ -2,13 +2,13 @@ const { isEmpty } = require("lodash");
 const { v4 } = require("uuid");
 const db = require("../../connectors/db");
 const roles = require("../../constants/roles");
-const {getSessionToken}=require('../../utils/session')
+const { getSessionToken } = require('../../utils/session')
 const getUser = async function (req) {
   const sessionToken = getSessionToken(req);
   if (!sessionToken) {
     return res.status(301).redirect("/");
   }
-  console.log("hi",sessionToken);
+  console.log("hi", sessionToken);
   const user = await db
     .select("*")
     .from("se_project.sessions")
@@ -23,7 +23,7 @@ const getUser = async function (req) {
       "se_project.users.roleid",
       "se_project.roles.id"
     )
-   .first();
+    .first();
 
   console.log("user =>", user);
   user.isNormal = user.roleid === roles.user;
@@ -33,173 +33,138 @@ const getUser = async function (req) {
   return user;
 };
 
+
 module.exports = function (app) {
-  // example
-  module.exports = function (app) {
-    // example
-    app.get("/users", async function (req, res) {
-      // Existing code for retrieving users
-      // ...
-    });
-  
-    // Register HTTP endpoint for ticket refund
-    app.post("/api/v1/refund/:ticketId", async function (req, res) {
-      // Retrieve the ticket ID from the URL parameter
+
+  app.post("/api/v1/refund/:ticketId", async function (req, res) {
+    console.log("i am hereeee");
+    try {
+      const user = await getUser(req);
       const { ticketId } = req.params;
-  
-      // Retrieve the user from the session or authentication token
-      const user = await getUser(req);
-  
-      if (!user) {
-        return res.status(401).send("User not authenticated");
-      }
-  
-      try {
-        // Retrieve the ticket from the database
-        const ticket = await db
-          .select("*")
-          .from("se_project.tickets")
-          .where("id", ticketId)
-          .first();
-  
-        if (!ticket) {
-          return res.status(404).send("Ticket not found");
-        }
-  
-        // Check if the ticket belongs to the requesting user
-        if (ticket.userid !== user.id) {
-          return res.status(403).send("Access denied");
-        }
-  
-        // Perform the refund process here...
-        // Update the ticket status, calculate refund amount, etc.
-  
-        return res.status(200).send("Ticket refunded successfully");
-      } catch (e) {
-        console.log(e.message);
-        return res.status(400).send("Could not process ticket refund");
-      }
-    });
-  };
+      console.log(ticketId);
+      // Retrieve the ticket from the database
+      const ticket = await db
+        .select("*")
+        .from("se_project.tickets")
+        .where("id", ticketId)
+        .first();
 
-  module.exports = function (app) {
-    // example
-    app.get("/users", async function (req, res) {
-      // Existing code for retrieving users
-      // ...
-    });
-  
-    // Register HTTP endpoint for requesting senior assistance
-    app.post("/api/v1/senior/request", async function (req, res) {
-      // Retrieve the user from the session or authentication token
-      const user = await getUser(req);
-  
-      if (!user) {
-        return res.status(401).send("User not authenticated");
+      console.log(ticket);
+      if (!ticket) {
+        return res.status(404).send("Ticket not found");
       }
-  
-      // Extract the nationalId from the request body
+
+      // Check if the ticket belongs to the requesting user
+      if (ticket.userid !== user.id) {
+        return res.status(403).send("Access denied");
+      }
+
+      // Check if the ticket is a future dated ticket
+      const now = new Date();
+      const ticketDate = new Date(ticket.tripdate);
+      if (ticketDate.getTime() <= now.getTime()) {
+        return res.status(400).send("Cannot refund past dated tickets");
+      }
+
+      // Perform the refund process here...
+      // Update the ticket status, calculate refund amount, etc.
+      // For example:
+      console.log("here subid ",ticket.subid);
+      const subscription = await db
+        .select("*")
+        .from("se_project.subsription")
+        .where("id", ticket.subid)
+        .first()
+      console.log(subscription);
+
+      if (subscription != null) {
+        const newNoOfTickets = subscription.nooftickets + 1;
+        await db("se_project.subsription")
+          .select("*")  
+          .where("id", ticket.subid)
+          .update({
+            nooftickets: newNoOfTickets,
+          });
+      }
+      else{
+        // do the online logic
+      }
+
+
+      await db
+        .from("se_project.tickets")
+        .where("id", ticketId)
+        .del();
+
+
+      // Return success response
+      return res.status(200).send("Ticket refunded successfully");
+    } catch (e) {
+      console.log(e.message);
+      return res.status(400).send("Could not process ticket refund");
+    }
+  });
+
+
+
+  // Register HTTP endpoint for requesting senior assistance
+  app.post("/api/v1/senior/request", async function (req, res) {
+    try {
+      const user = await getUser(req);
       const { nationalId } = req.body;
-  
-      if (!nationalId) {
-        return res.status(400).send("National ID is required");
-      }
-  
-      try {
-        // Perform the request senior process here...
-        // You can implement your own business logic to handle the request,
-        // such as validating the national ID, notifying senior staff, updating user's request status, etc.
-        // You can use the user object to access user information.
-  
-        // Example: Validate the national ID format
-        // You can replace this with your own validation logic
-        const nationalIdRegex = /^\d{10}$/;
-        if (!nationalIdRegex.test(nationalId)) {
-          return res.status(400).send("Invalid national ID format");
-        }
-  
-        // Example: Notify senior staff about the request
-        // Replace this with your own notification mechanism
-        notifySeniorStaff(user, nationalId);
-  
-        // Example: Update user's request status in the database
-        // Replace this with your own database update logic
-        await updateRequestStatus(user.id, nationalId);
-  
-        return res.status(200).send("Request sent successfully");
-      } catch (e) {
-        console.log(e.message);
-        return res.status(400).send("Could not process the request");
-      }
-    });
-  };
+      console.log("heree");
+      console.log(nationalId);
+      await db("se_project.senior_requests").insert({
+        status: "pending",
+        nationalid: nationalId,
+        userid: user.id
+      })
 
-  module.exports = function (app) {
-    // example
-    app.get("/users", async function (req, res) {
-      // Existing code for retrieving users
-      // ...
-    });
-  
-    // Register HTTP endpoint for simulating a ride
-    app.put("/api/v1/ride/simulate", async function (req, res) {
-      // Retrieve the user from the session or authentication token
-      const user = await getUser(req);
-  
-      if (!user) {
-        return res.status(401).send("User not authenticated");
-      }
-  
-      // Extract the origin, destination, and trip date from the request body
+      return res.status(200).send("Request sent successfully");
+    } catch (e) {
+      console.log(e.message);
+      return res.status(400).send("Could not process the request");
+    }
+  });
+
+  app.put("/api/v1/ride/simulate", async function (req, res) {
+
+    try {
       const { origin, destination, tripDate } = req.body;
-  
-      if (!origin || !destination || !tripDate) {
-        return res.status(400).send("Origin, destination, and trip date are required");
-      }
-  
-      try {
-        // Perform the ride simulation here...
-        // You can implement your own business logic to simulate the ride,
-        // such as validating the station names, calculating the trip duration, updating user's ride history, etc.
-        // You can use the user object to access user information.
-  
-        // Example: Validate the station names
-        // You can replace this with your own validation logic
-        if (!isValidStationName(origin) || !isValidStationName(destination)) {
-          return res.status(400).send("Invalid station name");
-        }
-  
-        // Example: Calculate trip duration
-        // Replace this with your own calculation logic based on the given origin, destination, and trip date
-  
-        const tripDuration = calculateTripDuration(origin, destination, tripDate);
-  
-        // Example: Update user's ride history in the database
-        // Replace this with your own database update logic
-        await updateRideHistory(user.id, origin, destination, tripDate, tripDuration);
-  
-        return res.status(200).send("Ride simulated successfully");
-      } catch (e) {
-        console.log(e.message);
-        return res.status(400).send("Could not simulate the ride");
-      }
-    });
-  };
-  
+      const status = "completed";
+      // const createRide = await db("se_project.rides").insert({
+      //   status,
+      //   origin,
+      //   destination,
+      //   tripDate
+      // })
+      const rideUpdate = await db("se_project.rides")
+        .where({
+          "origin": origin,
+          "destination": destination,
+          "tripdate": tripDate
+        })
+        .update({
+          status: "completed",
+        });
+
+      return res.status(200).send("Ride created and completed successfuly");
+    } catch (e) {
+      console.log(e.message);
+      return res.status(400).send("Could not process the ride or create");
+    }
+  });
+
   app.get("/users", async function (req, res) {
     try {
-       const user = await getUser(req);
+      const user = await getUser(req);
       const users = await db.select('*').from("se_project.users")
-        
+
       return res.status(200).json(users);
     } catch (e) {
       console.log(e.message);
       return res.status(400).send("Could not get users");
     }
-   
+
   });
- 
-
-
-  
-};
+}
