d30389c4dc077d97dfe94016b37e6c636495d828
amirwessam <121111555+amirwessam@users.noreply.github.com>
2023-05-26 19:18:47 +0300

    Check price, //Accept/Reject Senior Request,//senior request:request for senior role,get zones data

--- routes/private/api.js
+++ routes/private/api.js
@@ -58,104 +58,6 @@ module.exports = function (app) {
       return res.status(400).send("Could not get zones");
     }
   });
-  //subscriptions:Pay for subscription online
-  //not working
-  app.post("/api/v1/subscription", async function (req, res) {
-    try {
-      const {
-        subtype,
-        zoneid,
-        userid,
-        nooftickets
-      } = req.body;
-  
-      if (!subtype || !zoneid || !userid || !nooftickets) {
-        return res.status(400).send("All fields are required.");
-      }
-  
-      const subscription = await db("se_project.subscription")
-        .insert({
-          subtype,
-          zoneid,
-          userid,
-          nooftickets
-        })
-        .returning("*")
-        .then((rows) => rows[0]);
-  
-      return res.status(201).json(subscription);
-    } catch (err) {
-      console.error(err);
-      return res.status(500).send("Internal Server Error");
-    }
-  });
-  
-
-      
-
-
-  //tickets:Pay for ticket online
-  app.post("/api/v1/payment/ticket", async function (req, res) {
-    try {
-      const user = await getUser(req);
-      const { paymentMethodId, priceId } = req.body;
-      const customer = await db
-        .select("*")
-        .from("se_project.customers")
-        .where("userid", user.id)
-        .first();
-      if (!customer) {
-        return res.status(400).send("Customer not found");
-      }
-      const paymentIntent = await stripe.paymentIntents.create({
-        customer: customer.stripeid,
-        payment_method: paymentMethodId,
-        amount: 1000,
-        currency: "EGP",
-        confirmation_method: "manual",
-        confirm: true,
-      });
-      return res.status(200).json(paymentIntent);
-    } catch (e) {
-      console.log(e.message);
-      return res.status(400).send("Could not create payment intent");
-    }
-  });
-  //tickets:Pay for ticket by subscription
-  app.post("/api/v1/tickets/purchase/subscription", async function (req, res) {
-    try {
-      const user = await getUser(req);
-      const { subscriptionId } = req.body;
-      const customer = await db
-        .select("*")
-        .from("se_project.customers")
-        .where("userid", user.id)
-        .first();
-      if (!customer) {
-        return res.status(400).send("Customer not found");
-      }
-      const subscription = await stripe.subscriptions.retrieve(subscriptionId);
-      if (!subscription) {
-        return res.status(400).send("Subscription not found");
-      }
-      const ticket = await db
-        .insert({
-          id: v4(),
-          userid: user.id,
-          subscriptionid: subscription.id,
-          status: "active",
-          createdat: new Date(),
-          updatedat: new Date(),
-        })
-        .into("se_project.tickets")
-        .returning("*")
-        .then((rows) => rows[0]);
-      return res.status(200).json(ticket);
-    } catch (e) {
-      console.log(e.message);
-      return res.status(400).send("Could not purchase subscription");
-    }
-  });
   //prices:Check Price 
   //working
   app.get("/api/v1/tickets/price/:originId & :destinationId", async function (req, res) {
@@ -193,33 +95,7 @@ module.exports = function (app) {
       return res.status(400).send("Could not get price");
     }
   });
-
-  app.get("/users", async function (req, res) {
-    try {
-       const user = await getUser(req);
-      const users = await db.select('*').from("se_project.users")
-        
-      return res.status(200).json(users);
-    } catch (e) {
-      console.log(e.message);
-      return res.status(400).send("Could not get users");
-    }
-   
-  });
-  app.get("/api/v1/tickets", async function (req, res) {
-    try {
-      const user = await getUser(req);
-      const tickets = await db
-        .select("*")
-        .from("se_project.tickets")
-        .where("userid", user.id);
-      return res.status(200).json(tickets);
-    } catch (e) {
-      console.log(e.message);
-      return res.status(400).send("Could not get tickets");
-    }
-  }
-  );
+  
   //senior request:request for senior role
   //working
   app.post("/api/v1/senior/request", async function (req, res) {
@@ -242,5 +118,40 @@ module.exports = function (app) {
       res.status(500).send("An error occurred while creating the senior request.");
     }
   });
+  //Accept/Reject Senior Request
+  //working
+  app.put("/api/v1/requests/senior/:requestId", async function (req, res) {
+    try {
+      const { requestId } = req.params;
+      const { status } = req.body;
+      const parsedRequestId = parseInt(requestId, 10);
+      if (isNaN(parsedRequestId)) {
+        return res.status(400).send("Invalid requestId");
+      }
+  
+      const seniorRequest = await db
+        .select("*")
+        .from("se_project.senior_requests")
+        .where("id", parsedRequestId)
+        .first();
+  
+      if (!seniorRequest) {
+        return res.status(400).send("Senior request not found");
+      }
+  
+      const updatedSeniorRequest = await db
+        .update({ status })
+        .from("se_project.senior_requests")
+        .where("id", parsedRequestId)
+        .returning("*")
+        .then((rows) => rows[0]);
+  
+      return res.status(200).json(updatedSeniorRequest);
+    } catch (e) {
+      console.log(e.message);
+      return res.status(400).send("Could not update senior request");
+    }
+  });
+
 };
   
