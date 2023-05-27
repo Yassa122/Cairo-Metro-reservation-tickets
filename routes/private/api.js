66e8de52820b237ec09b8a88f9ba11013fdca85c
FadyIbrahim123 <fadyig125@gmail.com>
2023-05-27 20:52:04 +0300

    create and update station

--- routes/private/api.js
+++ routes/private/api.js
@@ -50,7 +50,10 @@ module.exports = function (app) {
       // Create the station in the database
       const station = {
         id: v4(), // Generate a unique ID for the station
-        name: stationName,
+        stationname: stationName,
+        stationtype: "normal",
+        stationposition: "start",
+        stationstatus: "new"
       };
 
       // Save the station in the database
@@ -62,6 +65,51 @@ module.exports = function (app) {
       return res.status(400).send("Could not create station");
     }
   });
+
+
+
+// Define the API endpoint for updating a station
+app.put("/api/v1/station/:stationId", async function (req, res) {
+  try {
+    // Extract the stationId and stationName from the request parameters and body
+    const { stationId } = req.params;
+    const { stationName } = req.body;
+
+    // Check if stationName is provided
+    if (!stationName) {
+      return res.status(400).json({ error: "Station name is required" });
+    }
+
+    // Update the station in the database
+    await db("se_project.stations")
+      .where({ id: stationId })
+      .update({ stationName });
+
+    return res.status(200).json({ message: "Station updated successfully" });
+  } catch (e) {
+    console.log(e.message);
+    return res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+// Define the API endpoint for deleting a station
+app.delete("/api/v1/station/:stationId", async function (req, res) {
+  try {
+    // Extract the stationId from the request parameters
+    const { stationId } = req.params;
+
+    // Delete the station from the database
+    await db("se_project.stations").where({ id: stationId }).del();
+
+    return res.status(200).json({ message: "Station deleted successfully" });
+  } catch (e) {
+    console.log(e.message);
+    return res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+
+
 
   app.get("/users", async function (req, res) {
     try {
