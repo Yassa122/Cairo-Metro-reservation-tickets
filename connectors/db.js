50c804acce05f723587878df17d062e7f7beed17
Yassa <99081198+Yassa122@users.noreply.github.com>
2023-06-10 14:39:15 +0300

    Update db.js

--- connectors/db.js
+++ connectors/db.js
@@ -1,11 +1,23 @@
+// import the knex library that will allow us to
+// construct SQL statements
 const knex = require('knex');
 
-const db = knex({
+// define the configuration settings to connect
+// to our local postgres server
+const config = {
   client: 'pg',
-  connection: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
+  connection: {
+    host: 'localhost',
+    port: 5432,
+    user: 'postgres',
+    password: 'Mankit123@',
+    database: 'se_project',
+  },
+};
+//qw
+// create the connection with postgres
+const db = knex(config);
 
+// expose the created connection so we can
+// use it in other files to make sql statements
 module.exports = db;
